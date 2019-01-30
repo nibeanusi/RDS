111e12dc4c7e189cfbefc7b6e7f022846ea8c63f
Nnenna Ibeanusi <nnennaibeanusi@MacBook-Pro-2018.local>
2019-01-29 21:11:44 -0800

    add test project

---
+++ test_project/rstudio_demo.R
@@ -0,0 +1,11 @@
+x <- c(1,2,3) #list of numbers 1, 2 and 3
+y <- 5:15 #similar to statement above but 5-15 inclusive
+mean(x)
+?mean #what is the mean #brings up the help file
+x <- c(0:10, 50)
+xm <- mean(x)
+xm
+x
+x <- c(1, 3, 5, 7, 9)
+y <- c(15, 16, 17, 18, 19)
+plot(x,y)
